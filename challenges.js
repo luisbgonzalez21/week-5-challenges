8b6f5a93572a296497ab5a1f129358f8e2c6ed34
luisbgonzalez21 <luisbg809@gmail.com>
2023-05-02 11:23:41 +0100

    first commit

---
+++ challenges.js
@@ -0,0 +1,8 @@
+const List = () => {
+    <div>
+        <a href="./hero-wiki"></a>
+        <a href="./number-button-challenge"></a>
+        <a href="./react-calculator"></a>
+        <a href="./react-image-clone-challenge"></a>
+    </div>
+}
