a4fb5bc61f6b532e3bd957be769db28648bc3d20
luisbgonzalez21 <luisbg809@gmail.com>
2023-05-02 20:24:57 +0100

    updated

--- challenges.js
+++ challenges.js
@@ -1,8 +1,10 @@
 const List = () => {
+    return(
     <div>
-        <a href="./hero-wiki"></a>
-        <a href="./number-button-challenge"></a>
-        <a href="./react-calculator"></a>
-        <a href="./react-image-clone-challenge"></a>
+        <a href="./hero-wiki">Hero Wiki</a>
+        <a href="./number-button-challenge">Number Button Challenge</a>
+        <a href="./react-calculator">Calculator</a>
+        <a href="./react-image-clone-challenge">Image Clone Challenge</a>
     </div>
+    )
 }
